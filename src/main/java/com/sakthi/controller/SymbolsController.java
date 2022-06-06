79bcbd8011c2e95b84909d32d48353f369f519f4
Sakthi Subramaniam <v-saksubramania@expediagroup.com>
2022-06-06 15:56:37 +0200

    controller with query parameters

--- src/main/java/com/sakthi/controller/SymbolsController.java
+++ src/main/java/com/sakthi/controller/SymbolsController.java
@@ -4,10 +4,12 @@ import com.sakthi.data.InMemoryStore;
 import com.sakthi.model.Symbol;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller("/symbols")
 public class SymbolsController {
@@ -26,5 +28,13 @@ public class SymbolsController {
     @Get("{value}")
     public Symbol getSymbolByValue(String value) {
         return inMemoryStore.getSymbols().get(value);
+    }
+
+    @Get("/filter{?max,offset}")
+    public List<Symbol> getSymbolsWithFilter(@QueryValue Optional<Integer> max, @QueryValue Optional<Integer> offset) {
+        return inMemoryStore.getSymbols().values().stream()
+                .skip(offset.orElse(0))
+                .limit(max.orElse(10))
+                .toList();
     }
 }
--- src/test/java/com/sakthi/controller/SymbolsControllerTest.java
+++ src/test/java/com/sakthi/controller/SymbolsControllerTest.java
@@ -11,9 +11,15 @@ import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 
 @MicronautTest
@@ -47,10 +53,34 @@ class SymbolsControllerTest {
         var testSymbol = new Symbol("TEST");
         inMemoryStore.getSymbols().put(testSymbol.value(), testSymbol);
 
-        var response = client.toBlocking().exchange("/"+testSymbol.value(), Symbol.class);
+        var response = client.toBlocking().exchange("/" + testSymbol.value(), Symbol.class);
         assertAll(
                 () -> assertEquals(HttpStatus.OK, response.status()),
                 () -> assertEquals(testSymbol, response.getBody().get())
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    @DisplayName("Symbols filter endpoint should return the exact count")
+    void testGetSymbolsWithFilter(Integer max, Integer offset, Integer expected) {
+        var response = client.toBlocking().exchange("/filter?max=" + max + "&offset=" + offset, JsonNode.class);
+        assertAll(
+                () -> assertEquals(HttpStatus.OK, response.status()),
+                () -> assertEquals(expected, response.getBody().get().size())
+        );
+    }
+
+    public static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                arguments(5, 5, 5),
+                arguments(2, 0, 2),
+                arguments(0, 5, 0),
+                arguments(5, 7, 3),
+                arguments(1, 1, 1),
+                arguments(null, 1, 9),
+                arguments(5, null, 5)
+        );
+    }
+
 }
