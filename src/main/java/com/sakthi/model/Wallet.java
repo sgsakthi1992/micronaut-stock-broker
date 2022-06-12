3b25cf54cd63aacbdf949e5bd2cd134f427804c2
Sakthi Subramaniam <v-saksubramania@expediagroup.com>
2022-06-12 20:38:20 +0200

    wallet controller

---
+++ src/main/java/com/sakthi/model/Wallet.java
@@ -0,0 +1,25 @@
+package com.sakthi.model;
+
+import com.sakthi.api.RestApiResponse;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record Wallet(
+        UUID accountId,
+        UUID walletId,
+        Symbol symbol,
+        BigDecimal available,
+        BigDecimal locked
+) implements RestApiResponse {
+
+    public Wallet addAvailable(BigDecimal amountToAdd) {
+        return new Wallet(
+                this.accountId,
+                this.walletId,
+                this.symbol,
+                this.available.add(amountToAdd),
+                this.locked
+        );
+    }
+}
